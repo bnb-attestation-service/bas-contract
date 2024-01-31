27a73bae41f81d72721f9ac8fb6725d3a40479de
stopWarByWar <nindelaofuqin@gmail.com>
2024-01-31 13:51:28 +0800

    rm useless contract

--- contracts/IIndexer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface IIndexer {
-    event Indexed(bytes32 indexed uid);
-    function indexAttestation(bytes32 attestationUID) external;
-}
